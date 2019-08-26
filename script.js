09da2059aa9ed5f0d684bdd52ad2f9950bc3c593
Amy <amyrogers390@gmail.com>
2019-08-26 23:53:50 +1200

    Switched to native js

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-//global variables
-let mapImg
-let issX = 0
-let issY = 0
-let latitude = 0
-let longitude = 0
+//get json file from NASA API, return data and call gotData
+function queryAPI() {
+    fetch("http://api.open-notify.org/iss-now.json")
+    .then(r => {
+        return r.json()
+    })
+    .then(data => {
+        gotData(data)
+        updatePosition()
+    })
+}
 
+//what to do after the data has returned
 function gotData(data) {
     longitude = data.iss_position.longitude
     latitude = data.iss_position.latitude
 
     //map the on-screen latitude and longitude to fit on the canvas
-    issX = map(longitude, -180, 180, 0, 632)
-    issY = map(latitude, 90, -90, 0, 399)
+    issX = scale(longitude, -180, 180, 0, 632)
+    issY = scale(latitude, 90, -90, 0, 399)
 }
 
-function askISS() {
-    //get json file, and when recieved that data, call gotData function
-    loadJSON("http://api.open-notify.org/iss-now.json", gotData)
+function updatePosition() {
+    //draw image (so only one dot appears on screen at once)
+    context.drawImage(map, 0, 0)
+
+    //draw dot
+    context.beginPath()
+    context.arc(issX, issY, 5, 0, 2 * Math.PI)
+    context.fillStyle = "red"
+    context.fill()
+    context.closePath()
+
+    //draw labels
+    context.font = "15px Arial"
+    context.fillText('ISS', issX + 10, issY)
+    context.font = "12px Arial"
+    context.fillText('latitude: ' + latitude, issX+10, issY+20)
+    context.fillText('longitude: ' + longitude, issX+10, issY+35)
 }
 
-//p5.js functions
-function preload() {
-    mapImg = loadImage("https://upload.wikimedia.org/wikipedia/commons/7/70/Arno_Peters-Projektion.JPG")
+//scale down real-life coordinates to fit on image
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers#23202637
+function scale (num, in_min, in_max, out_min, out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-function setup() {
-    setInterval(askISS, 5000) //call function every 5 seconds
-    createCanvas(632, 399)
-    fill('#ff0000')
-    stroke('#ff0000')
-}
+//global variables
+let issX = 0
+let issY = 0
+let latitude = 0
+let longitude = 0
+let canvas = document.getElementById("canvas")
+let context = canvas.getContext("2d")
+let map = document.getElementById("img")
 
-function draw() {
-    image(mapImg, 0, 0)
-    ellipse(issX, issY, 10, 10)
-
-    //display the labels next to the ISS dot
-    textSize(15)
-    text('ISS', issX + 10, issY)
-    textSize(12)
-    text('latitude: ' + latitude, issX+10, issY+20)
-    text('longitude: ' + longitude, issX+10, issY+35)
-}
+//query the API every 5 seconds
+setInterval(queryAPI, 5000)
